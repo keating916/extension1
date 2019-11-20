bce5ef89ed92708946d5f7946a81c3c84101f216
Patrick Keating <keating916@gmail.com>
2019-11-20 11:51:09 -0800

    Add rule to show only on Prifina.com

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+var rule1 = {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'www.prifina.com', schemes: ['https'] }
+          })
+        ],
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      };
+
+chrome.runtime.onInstalled.addListener(function(details) {
+        chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+          chrome.declarativeContent.onPageChanged.addRules([rule1]);
+        });
+      });
+
+
 /* 
 let string = {"key": "This is the default string"};
 chrome.runtime.onInstalled.addListener(function() {

77f5d2d96f315563047847bd2717eeb13b52984c
Patrick Keating <keating916@gmail.com>
2019-11-21 10:32:28 -0800

    add popup.js

---
+++ popup.js
@@ -0,0 +1,18 @@
+console.log("loaded");
+let ret = () => {
+    chrome.storage.local.get(['key'], function(result) {
+        console.log(`Returned string: ${result.key}`)
+    })
+}; //returns string set by set()
+
+let set = (string) => {
+    chrome.storage.sync.set({"key": string}, function() {
+        console.log("string has been reset")
+    })
+} //sets string
+
+document.getElementById("submitButton").addEventListener("click", e=> {
+	let s = document.getElementById("string");
+	set(s.value)
+	console.log(s.value);
+})

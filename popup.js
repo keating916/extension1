41e7dfcb68721bcc4f9e06ab931e1ef173ae74c7
Patrick Keating <keating916@gmail.com>
2019-11-21 21:36:39 -0800

    fix popup files reload bug

--- popup.js
+++ popup.js
@@ -2,20 +2,20 @@ console.log("loaded");
 
 let ret = () => {
     chrome.storage.sync.get(['key'], function(result) {
-        console.log(`Returned string: ${result.key}`)
+        console.log(`Saved String: ${result.key}`)
     })
 }; //returns string set by set()
 
 let set = (string) => {
     chrome.storage.sync.set({"key": string}, function() {
-        console.log("string has been reset")
+        console.log("string has been reset to:"+ string)
     })
 } //sets string
 
 document.getElementById("submitButton").addEventListener("click", e=> {
 	let s = document.getElementById("string");
-	set(s.value)
-	console.log(s.value);
+    set(s.value)
+    s.value="";
 })
 
 ret();
